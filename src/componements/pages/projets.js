4cdc9c2dce4540672f4024a9e48378bd12dbe525
Momo Taxi <gillan_a@etna-alternance.net>
2021-02-11 15:39:16 +0100

    add project section

--- src/componements/pages/projets.js
+++ src/componements/pages/projets.js
@@ -11,7 +11,7 @@ function projets() {
             <div className="projects" id="projets">
                 
                 <section className="section_projects" data-aos="fade-zoom-in" data-aos-delay="50"> 
-                    <a href="https://github.com/alxglt/E-commerce-Laravel" target="_blank">
+                    <a href="https://github.com/alxglt/E-commerce-Laravel" target="_blank" rel="noreferrer">
                         <img loading="lazy" src={laravel}  className="img" alt="my e-commerce website"/>
                         <p className="desc_projects">Création d'un site d'e-commerce avec laravel, avec un base de donnée en MySQL.</p>
                         <span className="technos">PHP </span>
@@ -22,7 +22,7 @@ function projets() {
                 </section>
                         
                 <section className="section_projects" data-aos="fade-zoom-in" data-aos-delay="80">
-                    <a href="https://github.com/alxglt/Clicker-Games-JS" target="_blank">
+                    <a href="https://github.com/alxglt/Clicker-Games-JS" target="_blank" rel="noreferrer">
                         <img loading="lazy" src={clicker_js} className="img" alt="my clicker game"/>
                         <p className="desc_projects">Création d'un clicker game sur le thème de notre choix en JavaScript. Utilisation du LocalStorage pour sauvegarder la partie et la reprendre plus tard.</p>
                         <span className="technos"> JavaScript </span>
@@ -30,7 +30,7 @@ function projets() {
                 </section>
 
                 <section className="section_projects" data-aos="fade-zoom-in" data-aos-delay="500">
-                    <a href="https://github.com/alxglt/my_portfolio" target="_blank">
+                    <a href="https://github.com/alxglt/my_portfolio" target="_blank" rel="noreferrer">
                         <img loading="lazy" src={portfolio} className="img" alt="my portfolio" />
                         <p className="desc_projects"> Le portfolio que vous regardez actuellement est un projet personel fait en reactJS</p>
                         <span className="technos">ReactJS</span>
